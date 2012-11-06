75172cb8caffed4a27274864eee1dac546fec9ad
Josh Rule <rsj28@georgetown.edu>
2012-11-06 13:14:22 +0700

    add d' reporting to eval..nce.m

--- evaluatePerformance.m
+++ evaluatePerformance.m
@@ -1,20 +1,21 @@
-function aucs = evaluatePerformance(x,y,cv,method)
+function [aucs,dprimes] = evaluatePerformance(x,y,cv,method,nFeatures)
+% [aucs,dprimes] = evaluatePerformance(x,y,cv,method,nFeatures)
     [nClasses,nTrainingExamples,nRuns] = size(cv);
     aucs = zeros(nClasses,nTrainingExamples,nRuns);
+    dprimes = zeros(nClasses,nTrainingExamples,nRuns);
     for iClass = 1:nClasses
         for iTrain = 1:nTrainingExamples
             parfor iRun = 1:nRuns
                 fprintf('%d %d %d\n',iClass,iTrain,iRun);
                 if(iscell(x))
-                    X = x{iClass,iTrain,iRun};
+                    X = x{iClass,iRun};
                 else
                     X = x;
                 end
-                aucs(iClass,iTrain,iRun) = auc(classifyResponses(X(chooseFeatures(X,y(iClass,:)),:),...
-                                                                 y(iClass,:),...
-                                                                 cv{iClass,iTrain,iRun},...
-                                                                 method),...
-                                               y(iClass,~cv{iClass,iTrain,iRun}));
+                classificationValues = classifyResponses(X(chooseFeatures(X(:,cv{iClass,iTrain,iRun}),y(iClass,cv{iClass,iTrain,iRun}),ones(1,20000),nFeatures),:),y(iClass,:),cv{iClass,iTrain,iRun},method);
+                classPredictions = (sign(classificationValues)+1)./2;
+                aucs(iClass,iTrain,iRun) = auc(classificationValues,y(iClass,~cv{iClass,iTrain,iRun}));
+                dprimes(iClass,iTrain,iRun) = dprime(sign(classPredictions),y(iClass,~cv{iClass,iTrain,iRun}),1,0);
             end
         end
     end
