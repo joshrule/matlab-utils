6252dc9f551ab0116567b5ca342a718ea9aa6b6c
Josh Rule <rsj28@georgetown.edu>
2012-12-03 02:01:12 -0500

    improve extractedPatch functionality
    - add location reporting
    - add ability to specify if patches come from inside or outside target

--- extractedPatches.m
+++ extractedPatches.m
@@ -1,5 +1,5 @@
-function [patches bandsChosen imgChosen sizesChosen locationsChosen] = extractedPatches(imgs, nPatchesPerSize, patchSizes, USECROPS)
-% [patches bandsChosen imgChosen sizesChosen locationsChosen] = extractedPatches(imgs, nPatchesPerSize, patchSizes, USECROPS)
+function [patches bandsChosen imgChosen sizesChosen locationsChosen] = extractedPatches(imgs, nPatchesPerSize, patchSizes, INTRA, EXTRA)
+% [patches bandsChosen imgChosen sizesChosen locationsChosen] = extractedPatches(imgs, nPatchesPerSize, patchSizes, INTRA, EXTRA)
 %
 % extracts patches of C1 activations at random locations for use as S2 features.
 %
@@ -7,7 +7,10 @@ function [patches bandsChosen imgChosen sizesChosen locationsChosen] = extracted
 % - nPatchesPerSize: a scalar, the number of patches to create per size
 % - patchSizes: a 3 x nPatchSizes array of patch sizes. Each column should hold
 %     [nRows; nCols; nOrientations]
-% - USECROPS: a logical, if true, use extract from cropped images, if available
+% - INTRA: a 3-value, if > 0, use intra-bounds information, if 0, don't care,
+%       if < 0, don't
+% - EXTRA: a 3-value, if > 0, use extra-bounds information, if 0, don't care,
+%       if < 0, don't
 %
 % - patches: a cell array of length nPatchSizes, with the cell at index i
 %     containing an [prod(patchSizes(:,i)) nPatchesPerSize] array representing
@@ -16,7 +19,8 @@ function [patches bandsChosen imgChosen sizesChosen locationsChosen] = extracted
 % - imgChosen: an [1 nPatchesTotal] array, the image selected for each patch
 % - sizesChosen: an [1 nPatchesTotal] array, the size selected for each patch
 
-if (nargin < 4) USECROPS = 0; end;
+if (nargin < 5) EXTRA = -1; end;
+if (nargin < 4) INTRA = -1; end;
 if (nargin < 3) patchSizes = [4 8 12; 4 8 12; 4 4 4]; end;
 if (nargin < 2) nPatchesPerSize = 500; end;
 
@@ -34,37 +38,41 @@ INCLUDEBORDERS = 0;
 % initialize Gabor filters
 [filterSizes,filters,c1OL,~] = initGabor(orientations, RFsize, div);
 
-% select patch source images and get their S1/C1 activations
-sourceImgs = imgs(ceil(rand(1,nPatchesTotal)*nImgs));
-parfor iImg = 1:nPatchesPerSize % reuse images for multiple patches
-    img = readImages(sourceImgs(iImg));
-    c1r{iImg} = C1(img{1},filters,filterSizes,c1Space,c1Scale,c1OL,...
-                   INCLUDEBORDERS);
-end
+% compute S1/C1 activations from images
+c1r = c1rFromCells(readImages(imgs),filters,filterSizes,c1OL,c1Space,c1Scale,INCLUDEBORDERS);
+GRAY =  1;
+CROP = -1;
+c1rBound = c1rFromCells(readImages(imgs,GRAY,CROP),filters,filterSizes,c1OL,c1Space,c1Scale,INCLUDEBORDERS);
 
 % select patches from random C1 bands
 count = 1;
 for iPatch = 1:nPatchesPerSize
-    if (mod(iPatch,100) == 0) fprintf('%d\n',iPatch); end;
-    b = c1r{iPatch};
     for iSize = 1:nPatchSizes
         foundBand = 0;
         while (~foundBand)
+            if (mod(count,100) == 0) fprintf('%d/%d\n',count,nPatchesTotal); end;
+            chosenImg = ceil(rand(1)*nImgs);
+            b = c1r{chosenImg};
             randBand = ceil(rand() * length(b));
             bandRows = size(b{randBand},1);
             bandCols = size(b{randBand},2);
             x = floor(rand()*(bandRows-patchSizes(1,iSize)))+1;
             y = floor(rand()*(bandCols-patchSizes(2,iSize)))+1;
-            if x > 0 && y > 0
+            bDiff = sign(abs(b{randBand}-c1rBound{chosenImg}{randBand}));
+            touchTarget = reshape(bDiff(x:x+patchSizes(1,iSize)-1,y:y+patchSizes(2,iSize)-1,:),[],1);
+            if x > 0 && y > 0 &&...
+              ~(INTRA > 0 && ~any(touchTarget)) &&...
+              ~(INTRA < 0 && any(touchTarget)) &&...
+              ~(EXTRA > 0 && ~any(~touchTarget)) &&...
+              ~(EXTRA < 0 && any(~touchTarget))
                 foundBand = 1;
                 bandsChosen(1,count) = randBand;
                 sizesChosen(1,count) = iSize;
-                imgChosen{1,count} = sourceImgs{iPatch};
+                imgChosen{1,count} = imgs{chosenImg};
                 locationsChosen(:,count) = [x; y];
                 count = count + 1;
+                patches{iSize}(:,iPatch) = reshape(b{randBand}(x:x+patchSizes(1,iSize)-1,y:y+patchSizes(2,iSize)-1,:),[],1);
             end
         end
-        p = b{randBand}(x:x+patchSizes(1,iSize)-1,y:y+patchSizes(2,iSize)-1,:);
-        patches{iSize}(:,iPatch) = p(:);
     end
 end
