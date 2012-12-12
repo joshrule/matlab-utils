b073c802146d9895467dfc62856839ec10a934c7
Josh Rule <rsj28@georgetown.edu>
2012-12-12 11:22:57 +0700

    refactor readImages

---
+++ grayImage.m
@@ -0,0 +1,16 @@
+function imgOut = grayImage(imgIn)
+% imgOut = grayImage(imgIn)
+%
+% convert an image array to grayscale
+%
+% imgIn:  an array representing an image
+%
+% imgOut: an array representing a grayscaled image
+
+    if size(imgIn,3) == 3
+        imgOut = double(rgb2gray(imgIn));
+    else
+        imgOut = double(imgIn)
+    end
+end
+
