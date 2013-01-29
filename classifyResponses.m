f76ee09fb0c91f29412108fb46996c975c7d7fb3
Josh Rule <rsj28@georgetown.edu>
2013-01-28 22:06:17 -0500

    add classifier options to classification

--- classifyResponses.m
+++ classifyResponses.m
@@ -1,27 +1,29 @@
-function classificationValues = classifyResponses(x,y,training,classifier)
+function classificationValues = classifyResponses(x,y,training,classifier,options)
 %
 % a set of classification values for binary classification over an image set
 %
 % x: an [nFeatures nExamples] array holding feature values
 % y: a vector of length nExamples, the class labels of examples in x
 % training: a logical vector of length nExamples, examples in training set = 1
-% classifier: a string, the classifier to use, "svm" or gentleboost
+% classifier: a string, the classifier to use, svm or gentleboost
 %
 % classificationValues: the classification estimates from the classifier
 
+    if (nargin < 5) options = []; end;
     trainX = x(:, training)';
     testX  = x(:,~training)';
     trainY = y( training)';
     testY  = y(~training)';
     switch lower(classifier)
       case {'svm','libsvm'}
-        model = svmtrain(trainY,trainX,'-q -t 0 -b 1');
+        if isempty(options) options = '-q -t 0 -b 1'; end;
+        model = svmtrain(trainY,trainX,options);
         [~,~,allVals] = svmpredict(testY,testX, model,'-b 1');
         classificationValues = allVals(:,2-trainY(1));
       case {'gb','gentleboost'}
-        nRounds = 100;
+        if isempty(options) options = 100; end;
         trainY = double(trainY).*2 - 1; % [0,1] -> [-1,1]
-        model = gentleBoost(trainX',trainY',nRounds);
+        model = gentleBoost(trainX',trainY',options);
         [~,classificationValues] = strongGentleClassifier(testX',model);
       otherwise
         classificationValues = [];
--- evaluatePerformance.m
+++ evaluatePerformance.m
@@ -1,4 +1,4 @@
-function [aucs,dprimes] = evaluatePerformance(x,y,cv,method,nFeatures)
+function [aucs,dprimes] = evaluatePerformance(x,y,cv,method,options,nFeatures)
 % [aucs,dprimes] = evaluatePerformance(x,y,cv,method,nFeatures)
     [nClasses,nTrainingExamples,nRuns] = size(cv);
     aucs = zeros(nClasses,nTrainingExamples,nRuns);
@@ -6,13 +6,12 @@ function [aucs,dprimes] = evaluatePerformance(x,y,cv,method,nFeatures)
     for iClass = 1:nClasses
         for iTrain = 1:nTrainingExamples
             parfor iRun = 1:nRuns
-                fprintf('%d %d %d\n',iClass,iTrain,iRun);
                 if(iscell(x))
                     X = x{iClass,iRun};
                 else
                     X = x;
                 end
-                classificationValues = classifyResponses(X(chooseFeatures(X(:,cv{iClass,iTrain,iRun}),y(iClass,cv{iClass,iTrain,iRun}),[],nFeatures),:),y(iClass,:),cv{iClass,iTrain,iRun},method);
+                classificationValues = classifyResponses(X(chooseFeatures(X(:,cv{iClass,iTrain,iRun}),y(iClass,cv{iClass,iTrain,iRun}),[],nFeatures),:),y(iClass,:),cv{iClass,iTrain,iRun},method,options);
                 classPredictions = (sign(classificationValues)+1)./2;
                 aucs(iClass,iTrain,iRun) = auc(classificationValues,y(iClass,~cv{iClass,iTrain,iRun}));
                 dprimes(iClass,iTrain,iRun) = dprime(sign(classPredictions),y(iClass,~cv{iClass,iTrain,iRun}),1,0);
