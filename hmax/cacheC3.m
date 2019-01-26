854d1611b43071249e5c9a7bd7c1d2a1b06067a6
Josh Rule <rsj28@georgetown.edu>
2019-01-26 06:13:09 -0500

    cacheC3 processes individually stored c2 activations

--- hmax/cacheC3.m
+++ hmax/cacheC3.m
@@ -1,19 +1,20 @@
-function imgFiles = cacheC3(outFile,c2File,modelFile,paramFile,models)
-% imgFiles = cacheC3(outFile,c2File,modelFile,paramFile,models)
+function c3 = cacheC3(c2Files,modelFile,paramFile,models)
+% cacheC3(c2Files,modelFile,paramFile,models)
 %
 % given c2Caches and a set of C3 models, write the C3 activations to disk
 %
-% outFile: string, the location in which to write the C3 activations to disk
-% c2File: string, the location of the C2 responses on disk
+% c2Files: string, the locations of the C2 responses on disk
 % modelFile: string, the location of the C3 units on disk
 % paramFile: string, the location of the parameters which generated the C3 units
 % models: cell array of classifiers, the models themselves, to prevent massive
 %   cache thrashing from loading and reloading the models
-%
-% imgFiles: cell array of strings, the images processed
-    if (nargin < 5) load(modelFile,'models'); end;
-    load(paramFile);
-    load(c2File,'imgFiles','c2');
-    c3 = testC3(c2,models,params.method);
-    save(outFile,'c3','imgFiles','modelFile','paramFile');
+    if (nargin < 4)
+        fprintf('loading models');
+        load(modelFile,'models');
+    end;
+    for i = 1:length(c2Files)
+        activations = load(c2Files{i},'c2','-mat');
+        c2(:,i) = activations.c2;
+    end
+    c3 = testC3(c2,models);
 end
