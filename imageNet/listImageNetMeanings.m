3e79e99fc4b05c659d4b23176ccfc4fcb3531e3f
Josh Rule <joshua.s.rule@gmail.com>
2019-10-01 06:23:49 -0400

    Image Net: update listImageNetMeanings.m

--- imageNet/listImageNetMeanings.m
+++ imageNet/listImageNetMeanings.m
@@ -1,8 +1,9 @@
 function listImageNetMeanings(wnids,srFile,outFile)
-    fid = fopen(outFile,'w');
     for i = 1:length(wnids)
         w = wnidToDefinition(srFile,wnids{i});
         fprintf('%d :: %s :: %s :: %s\n',i,wnids{i},w.words,w.gloss);
-        fprintf(fid,'%s :: %s :: %s\n',wnids{i},w.words,w.gloss);
+        words{i} = w.words;
+        glosses{i} = w.gloss;
     end
-    fclose(fid);
+    wnidTable = table(wnids,words',glosses','VariableNames', {'wnid','word','gloss'});
+    writetable(wnidTable, outFile, 'WriteVariableNames', true, 'Delimiter',',', 'QuoteStrings',true);
